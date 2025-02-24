3d387dceae518c314874c89eca36085a51c14186
susan kandel 1 <susankandel99@gmail.com>
2025-02-24 17:58:19 +0545

    area of cuboid import from cuboid.js to main.js

---
+++ cuboid.js
@@ -0,0 +1,6 @@
+const calculateCuboidAread = (l = 1, b = 1, h = 1) => l * b * h;
+ 
+module.exports = {
+  calculateCuboidAread,
+};
+ 
--- main.js
+++ main.js
@@ -9,3 +9,7 @@ console.log(result);
 //Create a nord module for calculating the 
 // area of cuboid 
 // and import it to main.js file
+
+const { calculateCuboidAread } = require("./cuboid");
+const area = calculateCuboidAread(5, 6, 7);
+console.log(area);
