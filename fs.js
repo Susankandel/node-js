77626af94a863bbc31b3f6a4e9528ff1e7e48e9d
susan kandel 1 <susankandel99@gmail.com>
2025-02-24 17:51:59 +0545

    connect files

--- fs.js
+++ fs.js
@@ -1,4 +1,37 @@
+//express server
+//nodemodule
+//node architecture
+
 // Write a nodejs function to read the content from the file and show in browser
 
-const fs = require("fs")
-const http = require("http")
+const fs = require("fs");
+const http = require("http");
+const crypto = require("crypto");
+
+
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200, {
+//         "content-type": "application/json"
+//     });
+//     const data = fs.readFileSync(`./text.txt`).toString();
+//     res.end(`The content of the file are : ${result}`);
+// })
+
+// server.listen(8000);
+// console.log("Server is running on http://localhost:8000");
+
+// write a OTP generator function that generate 6 digit number
+//math.random()
+// crypto module from node  
+
+const generateOTP = (digits = 6) => {
+    const minNum = Math.pow(10, digits -1)
+    const maxNum = Math.pow(10, digits) -1;
+    const result =crypto.randomInt(minNum,maxNum)
+    return result;
+
+
+};
+
+console.log(generateOTP(5));
---
+++ main.js
@@ -0,0 +1,11 @@
+// const blogs = require("./my-own")
+
+//Destructure
+const { generateDescription } = require("./my-own")
+
+blogs.generateDescription("susan is sowtware enginner");
+console.log(result);
+
+//Create a nord module for calculating the 
+// area of cuboid 
+// and import it to main.js file
